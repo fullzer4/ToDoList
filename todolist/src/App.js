4bbb0f1966cbe35bd81a875eabaf107d1840ced8
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-03 08:24:59 -0300

    attt

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component} from 'react';
 import "./styles/reset.css";
 import "./styles/App.css";
+import Titulo from "./components/titulo/titulo"
 
 class App extends Component {
     render() {
         return(
             <div id="pagina">
-                <h1>Sexo</h1>
+                <Titulo/>
             </div>
         )
     }
