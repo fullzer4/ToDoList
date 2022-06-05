d41858d7b1834673de03893b4683a786106770fa
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-06-05 02:57:12 -0300

    att

--- todolist/src/routes/routes.js
+++ todolist/src/routes/routes.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom"
-import { PrivateRoutes } from "."
+import { PrivateRoutes } from "./index"
 import { Home } from "../pages/Home/index"
 import { Login } from "../pages/Login/index"
 
