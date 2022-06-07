3d5e647ee85d2f4863538f8205007786223ed1c3
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-07 10:05:11 -0300

    att

--- todolist/src/pages/SignIn/index.jsx
+++ todolist/src/pages/SignIn/index.jsx
@@ -2,7 +2,6 @@ import { useState } from "react"
 import {Link} from "react-router-dom"
 import "./signin.css"
 
-
 export default function SignIn() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
@@ -12,15 +11,17 @@ export default function SignIn() {
   }
 
   return (
-    <div>
+    <div id="pagina">
+      <div className="formulario">
+        <h1 id="Titulo">Sign in</h1>
+        <form onSubmit={handleSubmit} id="campos">
+          <input id="campos-inserir" type="text" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <input id="campos-inserir" type="password" placeholder="senha"  value={password} onChange={(e) => setPassword(e.target.value)}/>
+          <button type="submit">Acessar</button>
+        </form>
 
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <input type="password" placeholder="senha"  value={password} onChange={(e) => setPassword(e.target.value)}/>
-        <button type="submit">Acessar</button>
-      </form>
-
-      <Link to="/register">criar uma conta</Link>
+        <Link to="/register">criar uma conta</Link>
+      </div>
     </div>
   );
 }
