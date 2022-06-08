7cfae84ea894dacf2a82f30179e93a834b218c84
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-08 10:55:41 -0300

    att

--- todolist/src/pages/Dashboard/index.jsx
+++ todolist/src/pages/Dashboard/index.jsx
@@ -1,9 +1,24 @@
-import { useContext } from "react"
+import { useContext , useState} from "react"
 import { AuthContext } from "../../contexts/auth" 
 import "./dashboard.css"
 
 export default function Dashboard(){
+  const [titulo, setTitulo] = useState("")
+  const [tarefa, setTarefa] = useState("")
+  const [hora, setHora] = useState("")
+  const [dia, setDia] = useState("")
+
+
   const { signOut , user} = useContext(AuthContext)
+
+  function handlecreate(e){
+    e.preventDefault()
+    console.log(titulo);
+    console.log(tarefa);
+    console.log(hora);
+    console.log(dia);
+  }
+
   return(
     <div id="pagina-dashboard">
       <div className="Header-dashboard">
@@ -11,16 +26,16 @@ export default function Dashboard(){
         <h1 id="titulo-dashboard">ToDoList</h1>
         <h2 id="user-header">{user.nome}</h2>
       </div>
-      <form>
+      <form onSubmit={handlecreate}>
         <label>Titulo</label>
-        <input type="text"></input>
+        <input type="text" value={titulo} onChange={(e)=> setTitulo(e.target.value)}></input>
         <label>Tarefa</label>
-        <input type="text"></input>
+        <input type="text" value={tarefa} onChange={(e)=> setTarefa(e.target.value)}></input>
         <label>Hora</label>
-        <input type="time"></input>
+        <input type="time" value={hora} onChange={(e)=> setHora(e.target.value)}></input>
         <label>Dia</label>
-        <input type="date"></input>
-        <button>Criar Tarefa</button>
+        <input type="date" value={dia} onChange={(e)=> setDia(e.target.value)}></input>
+        <button type="submit">Criar Tarefa</button>
       </form>
     </div>
   )
